58917512909be271edd3a2596cfbd019ec19224f
rikodin3 <yash.roshan2712@gmail.com>
2025-06-19 09:07:00 +0530

    configuration details included

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCgQdb4_fxAyRMr4eW6_MMvKXxq1x5u0g4",
+  authDomain: "task-cards-5df0b.firebaseapp.com",
+  projectId: "task-cards-5df0b",
+  storageBucket: "task-cards-5df0b.firebasestorage.app",
+  messagingSenderId: "152537910395",
+  appId: "1:152537910395:web:cc15c86f27205724de26de"
+}; 
+
